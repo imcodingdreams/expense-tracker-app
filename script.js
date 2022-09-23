abd7a0e86faeade8fb06c8d054a45cba551763b9
imcodingdreams <naykavallejo@yahoo.com>
2022-09-23 13:56:34 -0700

    Fix new table row to display as a column

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
 //VARIABLES
+const tblBody = document.getElementById('tblBody')
 
 // EVENT LISTENERS
 document.getElementById('addExpenseBtn').addEventListener('click', displayNewExpense);
 
-//FUNCTIONS
-
+// //FUNCTIONS
 function displayNewExpense() {
   
-  // const newExpenseRow = document.createElement('tr');
-  // newExpenseRow.setAttribute('class', 'new-row');
+  const newExpenseRow = document.createElement('tr');
+  newExpenseRow.setAttribute('class', 'new-row');
 
   const paymentType = document.getElementById('paymentType').value;
   const newPaymentType = document.createElement('td');
   newPaymentType.setAttribute('class', 'new-payment-type');
   newPaymentType.innerHTML = paymentType;
 
-  const expenseName = document.getElementById('name').value;
+  const expenseName = document.getElementById('expenseName').value;
   const newExpName = document.createElement('td');
   newExpName.setAttribute('class', 'new-name');
   newExpName.innerHTML = expenseName;
 
-  const expenseDate = document.getElementById('date').value;
+  const expenseDate = document.getElementById('expenseDate').value;
   const newExpDate = document.createElement('td');
   newExpDate.setAttribute('class', 'new-date');
   newExpDate.innerHTML = expenseDate;
 
-  const expenseAmount = document.getElementById('amount').value;
+  const expenseAmount = document.getElementById('expenseAmount').value;
   const newExpAmount = document.createElement('td');
   newExpAmount.setAttribute('class', 'new-amount');
-  newExpAmount.innerHTML = expenseAmount;
+  newExpAmount.innerHTML = `$${expenseAmount}`;
 
   const deleteBtn = document.createElement('td');
-  deleteBtn.innerHTML = '<button> <i class="fa-regular fa-trash-can"></i> </button>';
+  deleteBtn.innerHTML = '<button class="delete-btn"> <i class="fa-regular fa-trash-can"></i> </button>';
+  deleteBtn.setAttribute('class', 'delete-btn');
   deleteBtn.addEventListener('click', deleteExpense);
 
+  tblBody.appendChild(newExpenseRow);
   tblBody.appendChild(newPaymentType);
   tblBody.appendChild(newExpName);
   tblBody.appendChild(newExpDate);
   tblBody.appendChild(newExpAmount);
   tblBody.appendChild(deleteBtn);
-}
+};
 
+// FIX DELETE EXPENSE FUNCTION
 function deleteExpense(e) {
   const deleteBtn = e.target;
   const newExpenseRow = deleteBtn.parentElement;
-  tblBody.removeChild(newExpenseRow);
+  deleteBtn.parentElement.remove(newExpenseRow);
   }
 
 
+// Nodes
+/* <trnewExpenseRow >
+  <td>newPaymentType</td>
+  <td>newExpName</td>
+  <td>newExpDate</td>
+  <td>newExpAmount</td>
+  <td>deleteBtn</td>
+    <i>trash can </i>
+</trnewExpenseRow> */
+
+
 // PSEUDOCODE
+
+
+// Create a variable to hold the new expense row
+// Append new row to body table
 
 // Create a variable to hold the type
 // Create a variable to hold the name
@@ -71,6 +89,7 @@ function deleteExpense(e) {
 // Set class to the new element paymentType
 // Grab the expenseAmount input value
 
+
 // EVENT LISTENERS
 // Create an eventlistener for the submit button
 
@@ -80,7 +99,6 @@ function deleteExpense(e) {
 // });
 
 
-
 //FUNCTIONS
 
 // When I click the button
@@ -88,8 +106,4 @@ function deleteExpense(e) {
 // appendChild newExpName
 // appendChild newExpDate
 // appendChild newExpAmount
-
-
-// Create a variable to hold the new expense row
-// Append new row to body table
 
