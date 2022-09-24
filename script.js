ae09dde3ef20a2e9571727a468bc9f14db900033
imcodingdreams <naykavallejo@yahoo.com>
2022-09-23 17:03:37 -0700

    script.js code refactoring

--- script.js
+++ script.js
@@ -5,60 +5,77 @@ const tblBody = document.getElementById('tblBody')
 document.getElementById('addExpenseBtn').addEventListener('click', displayNewExpense);
 
 // //FUNCTIONS
-function displayNewExpense() {
-  
-  const newExpenseRow = document.createElement('tr');
-  newExpenseRow.setAttribute('class', 'new-row');
-
+function createNewPaymentType() {
   const paymentType = document.getElementById('paymentType').value;
   const newPaymentType = document.createElement('td');
   newPaymentType.setAttribute('class', 'new-payment-type');
   newPaymentType.innerHTML = paymentType;
+  tblBody.appendChild(newPaymentType);
+}
 
+function createNewName() {
   const expenseName = document.getElementById('expenseName').value;
   const newExpName = document.createElement('td');
   newExpName.setAttribute('class', 'new-name');
   newExpName.innerHTML = expenseName;
+  tblBody.appendChild(newExpName);
+}
 
+function createNewDate() {
   const expenseDate = document.getElementById('expenseDate').value;
   const newExpDate = document.createElement('td');
   newExpDate.setAttribute('class', 'new-date');
   newExpDate.innerHTML = expenseDate;
+  tblBody.appendChild(newExpDate);
+}
 
+function createNewAmount() {
   const expenseAmount = document.getElementById('expenseAmount').value;
   const newExpAmount = document.createElement('td');
   newExpAmount.setAttribute('class', 'new-amount');
   newExpAmount.innerHTML = `$${expenseAmount}`;
+  tblBody.appendChild(newExpAmount);
+}
 
+function createDeleteBtn() {
   const deleteBtn = document.createElement('td');
   deleteBtn.innerHTML = '<button class="delete-btn"> <i class="fa-regular fa-trash-can"></i> </button>';
   deleteBtn.setAttribute('class', 'delete-btn');
-  deleteBtn.addEventListener('click', deleteExpense);
-
-  tblBody.appendChild(newExpenseRow);
-  tblBody.appendChild(newPaymentType);
-  tblBody.appendChild(newExpName);
-  tblBody.appendChild(newExpDate);
-  tblBody.appendChild(newExpAmount);
   tblBody.appendChild(deleteBtn);
-};
+  deleteBtn.addEventListener('click', deleteExpense);
+}
+
+function createNewRow() {
+  const newExpenseRow = document.createElement('tr');
+  newExpenseRow.setAttribute('class', 'new-row');
+  tblBody.appendChild(newExpenseRow);
+}
+
+function displayNewExpense() {
+  createNewRow();
+  createNewPaymentType();
+  createNewName();
+  createNewDate();
+  createNewAmount();
+  createDeleteBtn();
+}
 
 // FIX DELETE EXPENSE FUNCTION
 function deleteExpense(e) {
   const deleteBtn = e.target;
-  const newExpenseRow = deleteBtn.parentElement;
+  const newExpenseRow = deleteBtn.parentElement.parentElement;
   deleteBtn.parentElement.remove(newExpenseRow);
 };
 
 
 // Nodes
-/* <trnewExpenseRow >
-  <td>newPaymentType</td>
-  <td>newExpName</td>
-  <td>newExpDate</td>
-  <td>newExpAmount</td>
-  <td>deleteBtn</td>
-    <i>trash can </i>
+/*<trnewExpenseRow >
+    <td>newPaymentType</td>
+    <td>newExpName</td>
+    <td>newExpDate</td>
+    <td>newExpAmount</td>
+    <td>deleteBtn</td>
+      <i>trash can </i>
 </trnewExpenseRow> */
 
 
